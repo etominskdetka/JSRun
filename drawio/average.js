ceeee0860353d7ead7b5ba41e0842290ebf2b4e4
etominskdetka <9798dima@gmail.com>
2021-08-07 00:02:30 +0300

    feat: add equality test in average function

--- drawio/average.js
+++ drawio/average.js
@@ -1,9 +1,11 @@
 function average(a, b, c) {
-    if ((a > b && b > c) || (c > b && b > a)) {
-        return b;
-    } else if ((b > a && a > c) || (c > a && a > b)) {
-        return a;
-    } else {
-        return c;
-    }
+  if (a == b || a == c || b == c) {
+    console.log("Error");
+  } else if ((a > b && b > c) || (c > b && b > a)) {
+    return b;
+  } else if ((b > a && a > c) || (c > a && a > b)) {
+    return a;
+  } else {
+    return c;
+  }
 }
